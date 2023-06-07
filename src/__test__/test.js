22c37b60595d13530b612f6e90c2fcc9e692af16
nika.movchanskaya <nika.movchanskaya@gmail.com>
2023-06-07 21:58:59 +0200

    delete comments

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,15 +1,6 @@
 import Magician from '../magician.js';
 import Daemon from '../daemon.js';
 
-// const distance = 2;
-/* const mag = new Magician();
-console.log(mag.attack);
-mag.attack = 20;
-console.log(mag.attack);
-console.log(mag.stoned);
-mag.stoned = 1;
-console.log(mag.stoned); */
-
 test('test initial attack', () => {
   const mag = new Magician();
   expect(mag.attack).toBe(8);
